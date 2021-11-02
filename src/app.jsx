5b7f172624953854f5b82ad31331311ef101eed9
yooja <wogud758787@naver.com>
2021-11-02 18:06:11 +0900

    8.10

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,47 @@
-import { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './app.module.css';
+import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
-import Video_list from './components/video_list/video_list';
+import VideoDetail from './components/video_detail/video_detail';
 
-function App({youtube}) {
-	const [videos, setVideos] = useState([]);
-	const search = query => {
-		youtube
-			.search(query)
-			.then(videos => setVideos(videos));
-	}
-	useEffect(()=> {
-		youtube
-			.mostPopular() //
-			.then(videos => setVideos(videos));
-	}, []);
+function App({ youtube }) {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-	return (
-		<div className={styles.app}>
-			<SearchHeader onSearch={search}/>
-			<Video_list videos={videos} />
-		</div>
+  const selectVideo = video => {
+    setSelectedVideo(video);
+  };
 
-	);
+  const search = query => {
+    youtube
+      .search(query) //
+      .then(videos => setVideos(videos));
+  };
+
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then(videos => setVideos(videos));
+  }, []);
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? 'list' : 'grid'}
+          />
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
-import Youtube from './service/youtube'
+import Youtube from './service/youtube';
 
 const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 ReactDOM.render(
   <React.StrictMode>
-    <App youtube={youtube}/>
+    <App youtube={youtube} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
