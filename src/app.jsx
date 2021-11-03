fbcab08640cf0ac07be5eaa33240ccb7d78fc3c1
yooja <wogud758787@naver.com>
2021-11-03 13:53:42 +0900

    finish

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
@@ -12,20 +12,20 @@ function App({ youtube }) {
     setSelectedVideo(video);
   };
 
-  const search = query => {
+  const search = useCallback(query => {
 	setSelectedVideo(null);
 	youtube
 		.search(query) //
 		.then(videos => {
 			setVideos(videos)
 		});
-  };
+  }, [])
 
   useEffect(() => {
     youtube
       .mostPopular() //
       .then(videos => setVideos(videos));
-  }, []);
+  }, [youtube]);
   return (
 	<div className={styles.app}>
 		<SearchHeader onSearch={search} />
